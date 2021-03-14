e59bcb9729d28fa53e87c93323d025125f091fab
annaliarosed <annaliadestefano@gmail.com>
2021-03-14 10:58:58 +0200

    fix background photo

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -5,10 +5,16 @@ import Footer from "./Footer";
 import styles from "./HomePage.module.scss";
 import TopSection from "./TopSection";
 import TravelKits from "./TravelKits";
+import backgroundPhoto from "../images/backgroundPhoto.png";
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
+      <img
+        src={backgroundPhoto}
+        alt="background"
+        className={styles.backgroundPhoto}
+      />
       <TopSection />
       <TravelKits />
       <AddOns />
