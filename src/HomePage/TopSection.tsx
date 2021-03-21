a62cdb145ede4b4549ebb2bf47519662223b697f
annaliarosed <annaliadestefano@gmail.com>
2021-03-21 10:29:14 +0200

    mobile styling

--- src/HomePage/TopSection.tsx
+++ src/HomePage/TopSection.tsx
@@ -8,22 +8,24 @@ const TopSection = () => (
     <img src={logo} alt="logo" />
     <section className={styles.wrapper}>
       <img src={suitcase} alt="suitcase" />
-      <h1 className={styles.header}>Connoisseur's travel kit</h1>
-      <p>
-        For too long, connoisseurs wishing to have a proper pour of top-quality
-        bourbon, whiskey, armagnac, brandy, rum, tequila or otherwise have had
-        to either drink below their palates when on the road, or strategically
-        fill their travel bags and luggage with glassware and sample bottles
-        wrapped in t-shirts, kitchen towels, or worse. To this we say, “no
-        more.”
-      </p>
-      <p>
-        Whether for yourself or that devotee of quality potent potables in your
-        life, our must-have Connoisseur ’s Travel Kits are as practical as they
-        are impressive. Treat yourself and your spirit(s) of choice with the
-        dignity you both deserve, while leaving no doubt to those around you
-        that you aren’t a “Joe SixPack” or “Bottom Shelf Betty”.
-      </p>
+      <div className={styles.text}>
+        <h1 className={styles.header}>Connoisseur's travel kit</h1>
+        <p>
+          For too long, connoisseurs wishing to have a proper pour of
+          top-quality bourbon, whiskey, armagnac, brandy, rum, tequila or
+          otherwise have had to either drink below their palates when on the
+          road, or strategically fill their travel bags and luggage with
+          glassware and sample bottles wrapped in t-shirts, kitchen towels, or
+          worse. To this we say, “no more.”
+        </p>
+        <p>
+          Whether for yourself or that devotee of quality potent potables in
+          your life, our must-have Connoisseur ’s Travel Kits are as practical
+          as they are impressive. Treat yourself and your spirit(s) of choice
+          with the dignity you both deserve, while leaving no doubt to those
+          around you that you aren’t a “Joe SixPack” or “Bottom Shelf Betty”.
+        </p>
+      </div>
     </section>
   </div>
 );
